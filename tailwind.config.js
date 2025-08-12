5a18406ba1ca9dfa54dea6026a2d3c455ebf0613
Antonio Mojena <itshelloant@gmail.com>
2025-08-11 22:53:10 -0500

    tailwind config cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ const { heroui } = require("@heroui/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
@@ -17,5 +15,5 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [heroui(), require('@tailwindcss/line-clamp')]
+  plugins: [heroui()]
 }; 
